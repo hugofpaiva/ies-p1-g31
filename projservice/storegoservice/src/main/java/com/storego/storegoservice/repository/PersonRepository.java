2b6521f40700ee10e0355bb8ac2ab742e326f206
hugofpaiva <hugofpaiva@ua.pt>
2020-12-08 16:37:23 +0000

    Person repository and start of auth

--- projservice/storegoservice/src/main/java/com/storego/storegoservice/repository/PersonRepository.java
+++ projservice/storegoservice/src/main/java/com/storego/storegoservice/repository/PersonRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    public Integer countByEmail(String email);
 }
--- projservice/storegoservice/src/main/java/com/storego/storegoservice/services/PersonService.java
+++ projservice/storegoservice/src/main/java/com/storego/storegoservice/services/PersonService.java
@@ -2,10 +2,33 @@ package com.storego.storegoservice.services;
 
 import com.storego.storegoservice.exception.EtAuthException;
 import com.storego.storegoservice.model.Person;
+import com.storego.storegoservice.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface PersonService {
+import javax.transaction.Transactional;
+import java.util.regex.Pattern;
 
-    Person validatePerson(String email, String password) throws EtAuthException;
+@Service
+@Transactional
+public class PersonService {
+    @Autowired
+    private PersonRepository personRepository;
 
-    Person registerPerson(String firstName, String lastName, String email, String password) throws EtAuthException;
+    public Person validatePerson(String email, String password) throws EtAuthException{
+
+    }
+
+    public Person registerPerson(Person p) throws EtAuthException{
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        if (!pattern.matcher(p.getEmail()).matches())
+            throw new EtAuthException("Invalid email format");
+        Integer count = personRepository.countByEmail(p.getEmail());
+        if (count > 0)
+            throw new EtAuthException("Email already in use")
+        return personRepository.save(p);
+
+    }
+
+
 }
