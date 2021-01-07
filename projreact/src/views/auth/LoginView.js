fef3b664f148d33c453eed123c2f5321d6f8708b
hugofpaiva <hugofpaiva@ua.pt>
2021-01-07 03:11:19 +0000

    url for websockets

--- projreact/src/views/auth/LoginView.js
+++ projreact/src/views/auth/LoginView.js
@@ -60,6 +60,7 @@ const LoginView = () => {
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ username: email, password: password })
 		};
+		console.log(Url + '/api/login');
 		const response = await fetch(Url + '/api/login', requestOptions);
 		const data = await response.json();
 		
