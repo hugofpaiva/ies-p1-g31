0964ac5600c48b98263bc34eae20ced89c4833ff
hugofpaiva <hugofpaiva@ua.pt>
2021-01-09 02:03:21 +0000

    REST Api info

--- projservice/storegoservice/src/main/java/com/storego/storegoservice/configuration/SwaggerConfig.java
+++ projservice/storegoservice/src/main/java/com/storego/storegoservice/configuration/SwaggerConfig.java
@@ -3,16 +3,15 @@ package com.storego.storegoservice.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-import java.util.Collections;
 
 @Configuration
 @EnableSwagger2WebMvc
@@ -30,12 +29,11 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "StoreGO REST API",
-                "Used to access most of the services of StoreGO.",
-                "API TOS",
-                "Terms of service",
-                new Contact("IES_Project_G31", "www.ua.pt", "sample@ua.pt"),
-                "License of API", "API license URL", Collections.emptyList());
+        return new ApiInfoBuilder().title("StoreGO REST API")
+                .description("Used to access most of the services of StoreGO")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
     }
 }
