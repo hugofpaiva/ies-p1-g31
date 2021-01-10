e484cca27aa88ab9a5af35e4c6aad1d454bba7d3
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-01-10 16:18:27 +0000

    bug correction

--- projreact/src/views/product/ProductListView/index.js
+++ projreact/src/views/product/ProductListView/index.js
@@ -37,7 +37,7 @@ const ProductList = (props) => {
 	useEffect(() => {
 		updateProducts();
 		updateCategories();
-	}, [updateProducts]);
+	}, []);
 
 	async function updateProducts() {
 		const requestOptions = {
